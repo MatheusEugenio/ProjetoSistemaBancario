10557faf04735972d64c3a76d7f9ac23dbb50a4a
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-12-16 18:48:32 -0300

    Ajustes finais na classe Transacao.java

--- src/Service/Transacao.java
+++ src/Service/Transacao.java
@@ -1,27 +1,67 @@
 package Service;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Transacao {
 
-    private LocalDate data;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private LocalDate dataTransacao;
     private String tipo_de_transacao;
     private Double valTransacao;
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private final String caminhoDoRegistro;
+    private final File arquivoRegistrador;
 
 
-    public Transacao(String dt, String tipoTransacao, double val){
-        this.data = LocalDate.parse(dt, formatter);
+    public Transacao(String caminhoDoRegistro , String tipoTransacao, double val){
+        this.caminhoDoRegistro = caminhoDoRegistro;
         this.tipo_de_transacao = tipoTransacao;
         this.valTransacao = val;
+        this.arquivoRegistrador = new File(caminhoDoRegistro);
+
+        this.dataTransacao = LocalDate.now();
+        dataTransacao.format(formatter);
     }
 
-    public void registrar(){
+    public Transacao(String caminhoDoRegistro, String tipoTransacao, double val, LocalDate dataEspecifica){
+        this(caminhoDoRegistro, tipoTransacao, val); // Chama o construtor de cima
 
+        this.dataTransacao = dataEspecifica;
+        dataTransacao.format(formatter);
     }
 
-    public LocalDate getData() {return data;}
+    public void registrar() throws IOException {//falta a implementação das interfaces
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoDoRegistro,true))){
+            if (!arquivoRegistrador.exists() || arquivoRegistrador.length() == 0) {
+                writer.write("--- REGISTRO DE TRANSAÇÕES BANCÁRIAS ---");
+                writer.newLine();
+            }
+
+            writer.write("Data da Transação: " + dataTransacao +
+                        "| Tipo de transacao: "+ tipo_de_transacao +
+                        "| Valor da transacao: "+ valTransacao);
+            writer.newLine();
+
+            writer.write("---------------------------------------------");
+            writer.newLine();
+            writer.flush();
+        }catch (IOException e) {
+            throw new IOException("FALHA AO REGISTRAR AS TRANSAÇÕES NO BANCO!");
+        }
+
+        System.out.println("--- REGISTRO DE TRANSAÇÕES BANCÁRIAS ---");
+        System.out.println("Data da Transação: " + dataTransacao);
+        System.out.println("Tipo de Transação: " + tipo_de_transacao);
+        System.out.printf("Valor da Transação: %.2f%n", valTransacao);
+        System.out.println("---------------------------------------------");
+    }
+
+    public LocalDate getDataTransacao() {return dataTransacao;}
     public String getTipo_de_transacao() {return tipo_de_transacao;}
     public Double getValTransacao() {return valTransacao;}
 }
