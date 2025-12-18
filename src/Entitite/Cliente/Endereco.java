ef60606108866a5cd8345e63824e36b73ad5a1bc
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-12-18 17:08:52 -0300

    implementação do novo uso de Endereco como classe no código

---
+++ src/Entitite/Cliente/Endereco.java
@@ -0,0 +1,35 @@
+package Entitite.Cliente;
+
+public class Endereco {
+    private String rua;
+    private Integer cep;
+    private String numeroDaCasa;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+
+    public Endereco(String rua, Integer cep, String numeroDaCasa, String complemento, String bairro, String cidade) {
+        this.rua = rua;
+        this.cep = cep;
+        this.numeroDaCasa = numeroDaCasa;
+        if (complemento.isBlank() || complemento == null) {this.complemento = "nenhum";
+        }else {this.complemento = complemento;}
+        this.bairro = bairro;
+        this.cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return " | Rua: " + rua +
+                " | Bairro: " + bairro +
+                " | Número Da Casa: " + numeroDaCasa +
+                " | Cidade: "+ cidade;
+    }
+
+    public String getRua() {return rua;}
+    public Integer getCep() {return cep;}
+    public String getNumeroDaCasa() {return numeroDaCasa;}
+    public String getComplemento() {return complemento;}
+    public String getBairro() {return bairro;}
+    public String getCidade() {return cidade;}
+}
--- src/Entitite/Cliente/TiposDeCliente/ClientePessoaFisica.java
+++ src/Entitite/Cliente/TiposDeCliente/ClientePessoaFisica.java
@@ -1,17 +1,18 @@
 package Entitite.Cliente.TiposDeCliente;
 
 import Entitite.Cliente.Cliente;
+import Entitite.Cliente.Endereco;
 
 public class ClientePessoaFisica extends Cliente {
 
-    private long cpf;
+    private String cpf;
 
-    public ClientePessoaFisica(String nome, long cpf, String endereco, String data_de_nascimento) {
+    public ClientePessoaFisica(String nome, String cpf, Endereco endereco, String data_de_nascimento) {
         super(nome, endereco, data_de_nascimento);
         this.cpf = cpf;
     }
 
-    public long getCpf() {return cpf;}
+    public String getCpf() {return cpf;}
 
     @Override
     public String toString() {
