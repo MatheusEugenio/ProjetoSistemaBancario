a1341bb2f90f96310209e0ea5cc8aafd99fb3ecb
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-12-20 16:46:22 -0300

    ajuste no cep, agora é long

--- src/entity/cliente/Endereco.java
+++ src/entity/cliente/Endereco.java
@@ -2,13 +2,13 @@ package entity.cliente;
 
 public class Endereco {
     private String rua;
-    private Integer cep;
+    private long cep;
     private int numeroDaCasa;
     private String complemento;
     private String bairro;
     private String cidade;
 
-    public Endereco(String rua, Integer cep, int numeroDaCasa, String complemento, String bairro, String cidade) {
+    public Endereco(String rua, long cep, int numeroDaCasa, String complemento, String bairro, String cidade) {
         this.rua = rua;
         this.cep = cep;
         this.numeroDaCasa = numeroDaCasa;
@@ -20,14 +20,14 @@ public class Endereco {
 
     @Override
     public String toString() {
-        return " | Rua: " + rua +
+        return "Rua: " + rua +
                 " | Bairro: " + bairro +
                 " | Número Da Casa: " + numeroDaCasa +
                 " | Cidade: "+ cidade;
     }
 
     public String getRua() {return rua;}
-    public Integer getCep() {return cep;}
+    public long getCep() {return cep;}
     public int getNumeroDaCasa() {return numeroDaCasa;}
     public String getComplemento() {return complemento;}
     public String getBairro() {return bairro;}
